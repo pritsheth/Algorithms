a7b6e44cbdc9c083c09819c00f929d70c076c3cf
prit_sheth <pritsheth99@gmail.com>
2017-11-05 15:11:55 -0700

    Updating array problems: buy sell stock problem

--- ArrayProblems/BuySellStock.py
+++ ArrayProblems/BuySellStock.py
@@ -1,5 +1,7 @@
 class Solution(object):
     def maxProfit(self, prices):
+        if len(prices) == 0:
+            return 0
         min = prices[0]
         profit = 0
         for i in range(1, len(prices)):
@@ -8,3 +10,14 @@ class Solution(object):
             if prices[i] - min > max:
                 profit = prices[i] - min
         return profit
+
+
+    # Best method to solve the mnaximumsub array problem.
+    def maxProfitKdane(self, prices):
+        max_cur = 0
+        max_global = 0
+        for i in range(1, len(prices)):
+            max_cur = + prices[i] - prices[i - 1]
+            max_cur = max(0, max_cur)
+            max_global = max(max_cur, max_global)
+        return max_global
