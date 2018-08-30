81472d2f812ee72031b4721164befefae3d7a2c4
pritsheth <pritsheth99@gmail.com>
2018-08-30 16:45:01 -0700

    Update LCS

--- DynamicProgramming/LCS.py
+++ DynamicProgramming/LCS.py
@@ -1,15 +1,17 @@
 # Longest common sub sequence:
 def find_length_lcs(a1, a2):
-    dp = [[0 for i in range(len(a1) + 1)] for j in range(len(a2) + 1)]
+    N = len(a1)
+    dp = [[0 for i in range(N + 1)] for j in range(N + 1)]
 
-    for i in range(1, len(a1) + 1):
-        for j in range(1, len(a2) + 1):
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
             if a1[i - 1] == a2[j - 1]:
                 dp[i][j] = 1 + dp[i - 1][j - 1]
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-    return dp[len(a1)][len(a1)]
+    print(dp)
+    return dp[N][N]
 
 
-print(find_length_lcs([1, 5, 2, 6, 3, 7], [5, 6, 7, 1, 2, 3]))
+print(find_length_lcs([2, 3, 1], [1, 2, 3]))
